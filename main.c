3e766de7582d1a1a7a9ceb90366f50636d367908
Lucas Nadler <lucasnadlerrocha@hotmail.com>
2020-05-19 21:26:51 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(){
     printf("Hello World!\n");
     printf("Lucas Nadler é preguiçoso!\n");
     printf("Agora vai, deu certo?\n");
-    printf("Update");
+    int i=9;
     if(i==9){
         printf("i eh igual a 9\n");
     }
