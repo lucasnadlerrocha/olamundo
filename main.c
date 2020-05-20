50518bafa94de2ebcdda824354b0a0e1a2890b84
Lucas Nadler <lucasnadlerrocha@hotmail.com>
2020-05-19 21:07:35 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(){
+    printf("====TESTES====="); //TESTES OK?
     printf("Hello World!\n");
     printf("Lucas Nadler é preguiçoso!\n");
     printf("Agora vai, deu certo?\n");
